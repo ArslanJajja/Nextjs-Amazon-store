e5a8930d4acf90e34177fe474e048ebf82a0ba88
Arslan Jajja <arslanjajja1@gmail.com>
2021-08-01 16:38:21 +0500

    Header complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,8 +7,9 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-
-      
+    {/* Header */}
+    <Header />
+        
     </div>
   );
 }
