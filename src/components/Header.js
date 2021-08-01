732156ab142b0729e4e007d120edcd0e3f59dd4b
Arslan Jajja <arslanjajja1@gmail.com>
2021-08-01 18:07:16 +0500

    Banner Setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,40 +28,41 @@ function Header() {
         </div>
         {/* Right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-            <div className='link'>
-                <p>Hello Arslan Jajja</p>
-                <p className='font-extrabold md:text-sm'>Account & Lists</p>
-            </div>
+          <div className="link">
+            <p>Hello Arslan Jajja</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
 
-            <div className='link'>
-                <p>Returns</p>
-                <p className='font-extrabold md:text-sm'>& Orders</p>
-            </div>
-            <div className='relative link flex items-center'>
-              <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
-                <ShoppingCartIcon className="h-10"/>
-                <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
-            </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
         </div>
       </div>
       {/* Bottom Nav */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
-          <MenuIcon className="h-6 mr-1"/>
+          <MenuIcon className="h-6 mr-1" />
           All
         </p>
-        <p className='link'>Prime Video</p>
-        <p className='link'>Amazon Business</p>
-        <p className='link'>Today's Deals</p>
-        <p className='link hidden lg:inline-flex'>Electronics</p>
-        <p className='link hidden lg:inline-flex'>Food & Grocery</p>
-        <p className='link hidden lg:inline-flex'>Prime</p>
-        <p className='link hidden lg:inline-flex'>Buy Again</p>
-        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
-        <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
-
-
-
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
---
+++ src/components/ProductFeed.js
@@ -0,0 +1,9 @@
+function ProductFeed() {
+    return (
+        <div>
+            <h1>Products here ...</h1>
+        </div>
+    )
+}
+
+export default ProductFeed
